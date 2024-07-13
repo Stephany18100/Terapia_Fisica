b11d6a84dae962e3c37cd745232e040ac6095390
Tapia-GJ <luivstapia285@gmail.com>
2024-07-13 16:45:54 -0500

    appcommit

--- app/app.py
+++ app/app.py
@@ -7,5 +7,33 @@ def index():
     # return "Hola mundo!"
     return render_template('ExploracionFisicaMiemSuper.html')
 
+<<<<<<< HEAD
+=======
+@app.route('/CabezayTorax')
+def CabezayTorax():
+    return render_template('ExploracionFisicaCabezaYtorax.html')
+
+@app.route('/PersoNoPatologicos')
+def PersoNoPatologicos():
+    return render_template('AtecedentesPersonalesNoPatologicos.html')
+
+@app.route('/AnteGinecobstetricos')
+def AnteGinecobstetricos():
+    return render_template('AnteGinecobstetricos.html')
+
+@app.route('/PersoPatologicos')
+def PersoPatologicos():
+    return render_template('AntecedentesPersonalesPatologicos.html')
+
+@app.route('/PadecimientoActual')
+def PadecimientoActual():
+    return render_template('AntecedentesPadecimientoActual.html')
+
+@app.route('/HeredoFam')
+def HeredoFam():
+    return render_template('AntecedentesHeredofamiliares.html')
+
+# hola
+>>>>>>> db1b41a (appcommit)
 if __name__ =='__main__' : 
     app.run(debug=True, port=5000)
