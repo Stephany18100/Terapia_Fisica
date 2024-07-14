1cffd1a891e95da056326b2988ecba30db06142d
Stephany18100 <fannynunez099@gmail.com>
2024-07-14 01:55:57 -0500

    Cambios

--- app/app.py
+++ app/app.py
@@ -4,8 +4,44 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return "Hola mundo!"
+    return render_template('index.html')
+
+@app.route('/fichaIdentificacion')
+def fichaIdentificacion():
+    return render_template('FichaIdentificacio.html')
+
+@app.route('/MiemInferior')
+def MiemInferior():
+    return render_template('ExploracionFisicaMiemInferior.html')
+
+@app.route('/MiemSuperior')
+def MiemSuperior():
     return render_template('ExploracionFisicaMiemSuper.html')
 
-if __name__ =='__main__' : 
+@app.route('/CabezayTorax')
+def CabezayTorax():
+    return render_template('ExploracionFisicaCabezaYtorax.html')
+
+@app.route('/PersoNoPatologicos')
+def PersoNoPatologicos():
+    return render_template('AtecedentesPersonalesNoPatologicos.html')
+
+@app.route('/AnteGinecobstetricos')
+def AnteGinecobstetricos():
+    return render_template('AnteGinecobstetricos.html')
+
+@app.route('/PersoPatologicos')
+def PersoPatologicos():
+    return render_template('AntecedentesPersonalesPatologicos.html')
+
+@app.route('/PadecimientoActual')
+def PadecimientoActual():
+    return render_template('AntecedentesPadecimientoActual.html')
+
+@app.route('/HeredoFam')
+def HeredoFam():
+    return render_template('AntecedentesHeredofamiliares.html')
+
+# hola
+if __name__ =='_main_' : 
     app.run(debug=True, port=5000)
