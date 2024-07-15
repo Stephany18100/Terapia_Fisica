286b98dd8c2f84e9ef096d0b4b43137519e0e2b8
Stephany18100 <fannynunez099@gmail.com>
2024-07-15 00:27:09 -0500

    Cambio en app.py

--- app/app.py
+++ app/app.py
@@ -42,6 +42,5 @@ def PadecimientoActual():
 def HeredoFam():
     return render_template('AntecedentesHeredofamiliares.html')
 
-# hola
-if __name__ =='_main_' : 
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
